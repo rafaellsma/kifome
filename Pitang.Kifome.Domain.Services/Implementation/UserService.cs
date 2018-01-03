b5c5ae6f0cedeacfbbf75d0e283012992bc44117
JoaoPauloLins <jplo@cin.ufpe.br>
2018-01-03 08:11:53 -0300

    Resolução de conflitos

--- Pitang.Kifome.Domain.Services/Implementation/UserService.cs
+++ Pitang.Kifome.Domain.Services/Implementation/UserService.cs
@@ -1,33 +1,50 @@
-﻿using Pitang.Kifome.Domain.Contracts.Services;
+﻿using Pitang.Kifome.Domain.Contracts.Repositories;
+using Pitang.Kifome.Domain.Contracts.Services;
+using Pitang.Kifome.Domain.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Pitang.Kifome.Domain.Entities;
 
 namespace Pitang.Kifome.Domain.Services.Implementation
 {
-    class UserService : IUserService
+    public class UserService : IUserService
     {
+        private readonly IUserRepository userRepository;
+        private readonly IOrderRepository orderRepository;
+
+        public UserService(IUserRepository userRepository, IOrderRepository orderRepository)
+        {
+            this.userRepository = userRepository;
+            this.orderRepository = orderRepository;
+        }
+
         public bool Authenticate(string email, string password)
         {
-            throw new NotImplementedException();
+            Person user = this.userRepository.SelectByEmail(email);
+            bool authenticate = false;
+            if (user != null)
+            {
+                authenticate = user.Password == password;
+            }
+            return authenticate;
         }
 
         public void CreateUser(Person person)
         {
-            throw new NotImplementedException();
+            userRepository.Insert(person);
         }
 
-        public void MakeComment(Person person, Conversation conversation, string mensage)
+        public void MakeComment(Person person, Order order, string mensage)
         {
-            throw new NotImplementedException();
+            Comment comment = new Comment(person, mensage);
+            order.Conversation.Comments.Add(comment);
         }
 
         public List<Order> OrdersFromUser(Person person)
         {
-            throw new NotImplementedException();
+            return orderRepository.SelectAllByPerson(person);
         }
     }
 }
