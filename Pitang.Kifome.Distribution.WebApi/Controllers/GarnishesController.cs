1c3b1f51f43150678a45af22c338b885fb3cba8a
Rafael Albuquerque <rafaellsma@gmail.com>
2018-01-12 12:53:07 -0200

    [WIP] Crud do garnish

--- Pitang.Kifome.Distribution.WebApi/Controllers/GarnishesController.cs
+++ Pitang.Kifome.Distribution.WebApi/Controllers/GarnishesController.cs
@@ -1,18 +1,24 @@
 ﻿using Pitang.Kifome.Application.Contracts.Services;
 using Pitang.Kifome.Application.Entities;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace Pitang.Kifome.Distribution.WebApi.Controllers
 {
     [RoutePrefix("api/garnish")]
+    //[EnableCors(origins: "*", headers: "*", methods: "*")]
     public class GarnishesController : ApiController
     {
+        #region Constructor
+
         public readonly ISellerAppService sellerAppService;
 
         public GarnishesController(ISellerAppService sellerAppService)
         {
             this.sellerAppService = sellerAppService;
         }
+
+        #endregion
 
         [AcceptVerbs("post")]
         public void CreateGarnish(GarnishInputDTO garnish)
