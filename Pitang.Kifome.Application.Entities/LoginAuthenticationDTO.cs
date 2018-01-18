935f7a5f5386a54ec5ce2eb8acb32529da8fb77a
JoaoPauloLins <jplo@cin.ufpe.br>
2018-01-18 16:28:14 -0200

    Ajuste nos DTO de User para restrição de senha

--- Pitang.Kifome.Application.Entities/LoginAuthenticationDTO.cs
+++ Pitang.Kifome.Application.Entities/LoginAuthenticationDTO.cs
@@ -12,7 +12,7 @@ namespace Pitang.Kifome.Application.Entities
         [Required]
         public string Email { get; set; }
         [Required]
-        [Range(6,25, ErrorMessage = "A senha deve conter no minimo 6 caracteres e no máximo 25)")]
+        [StringLength(25, MinimumLength = 6, ErrorMessage = "A senha deve conter no minimo 6 caracteres e no máximo 25)")]
         public string Password { get; set; }
 
     }
