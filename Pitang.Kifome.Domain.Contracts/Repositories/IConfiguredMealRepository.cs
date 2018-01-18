081f1c72545e24989abab4c4967247df6789208e
Rafael Albuquerque <rafaellsma@gmail.com>
2018-01-18 09:37:59 -0300

    Implementa configured meal repository

---
+++ Pitang.Kifome.Domain.Contracts/Repositories/IConfiguredMealRepository.cs
@@ -0,0 +1,13 @@
+﻿using Pitang.Kifome.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pitang.Kifome.Domain.Contracts.Repositories
+{
+    public interface IConfiguredMealRepository : IRepository<ConfiguredMeal, int>
+    {
+    }
+}
