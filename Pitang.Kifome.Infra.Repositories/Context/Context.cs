8fa145b3f970daff1512601caaf5a8f4066da2ca
Vinicius Santana <vinicius.santanna@hotmail.com>
2018-01-24 19:10:20 -0200

    OrderController

--- Pitang.Kifome.Infra.Repositories/Context/Context.cs
+++ Pitang.Kifome.Infra.Repositories/Context/Context.cs
@@ -4,7 +4,7 @@ namespace Pitang.Kifome.Infra.Repositories.Mapping
 {
     public class Context : DbContext
     {
-        public Context() : base("ConnKiFomePC09")
+        public Context() : base("ConnJeyJey")
         {
             Database.SetInitializer<Context>(null);
         }
